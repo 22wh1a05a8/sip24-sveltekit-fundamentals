741a794dc71e524ec0560b6a9255d9d59f3ae947
HimaVarshaK <147132110+22wh1a05a8@users.noreply.github.com>
2024-06-21 17:54:17 +0530

    Solution

---
+++ src/routes/add-post/+page.svelte
@@ -0,0 +1,13 @@
+<header class="bg-white py-4 shadow-md sticky top-0 z-10">
+    <div class="container mx-auto px-4 flex justify-between items-center">
+        <h1 class="text-2xl font-bold font-['Comic_Sans_MS']">Craftlab</h1>
+        <a href="/" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Home</a>
+    </div>
+</header>
+<form class = "container mx-auto p-5">
+    <input typr = "file"/>
+    <div>
+        <label for="username" class="block mb-2 text-sm font-medium text-gray-900">Username</label>
+        <input name="username" id="username" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5">
+    </div>
+</form>
