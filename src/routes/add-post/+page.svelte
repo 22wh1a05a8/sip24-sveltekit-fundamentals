8f81d954afa4d94151aeccf32fa26467f7ab5b00
HimaVarshaK <147132110+22wh1a05a8@users.noreply.github.com>
2024-06-21 17:59:59 +0530

    Sol

--- src/routes/add-post/+page.svelte
+++ src/routes/add-post/+page.svelte
@@ -6,8 +6,12 @@
 </header>
 <form class = "container mx-auto p-5">
     <input typr = "file"/>
-    <div>
+    <div calss="mb-3">
         <label for="username" class="block mb-2 text-sm font-medium text-gray-900">Username</label>
         <input name="username" id="username" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5">
+    </div>
+    <div calss="mb-3">
+        <label for="content" class="block mb-2 text-sm font-medium text-gray-900">Content</label>
+        <textarea name="content" id="content" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5">
     </div>
 </form>
